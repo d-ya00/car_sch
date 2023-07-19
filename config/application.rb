d2de0abce38bf03f05732c97b22e4d201ba676f9
d-ya00 <daiya.koi@gmail.com>
2023-07-19 14:54:23 +0900

    デバイスメッセージ日本語化

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module CarSch
     config.load_defaults 7.0
     config.i18n.available_locales = :ja
     config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
