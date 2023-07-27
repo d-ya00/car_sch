ff78e9061fa928678629b0cb7f8f37661b824cd0
d-ya00 <daiya.koi@gmail.com>
2023-07-28 06:48:57 +0900

    修正

--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,5 @@ Rails.application.routes.draw do
   resources :blogs, except: [:show] do
     get 'date/:date', to: 'blogs#show_by_date', on: :collection, as: 'show_by_date', constraints: { date: /\d{4}-\d{2}-\d{2}/ }
   end
-  resources :schedules, only: [:index]
+  
 end
