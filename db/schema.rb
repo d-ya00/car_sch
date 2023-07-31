dc8f6342330abf9cef59d5557f1820cd729bce92
d-ya00 <daiya.koi@gmail.com>
2023-07-31 23:27:53 +0900

    マイグレーションファイル順番変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,22 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_06_075024) do
-  create_table "blogs", charset: "utf8mb4", force: :cascade do |t|
+ActiveRecord::Schema[7.0].define(version: 2023_07_06_076335) do
+  create_table "blogs", charset: "utf8", force: :cascade do |t|
     t.string "title", null: false
     t.text "content"
     t.datetime "start_time"
     t.integer "price"
-
-=======
     t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-
     t.index ["user_id"], name: "index_blogs_on_user_id"
   end
 
-  create_table "users", charset: "utf8mb4", force: :cascade do |t|
+  create_table "users", charset: "utf8", force: :cascade do |t|
     t.string "name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
