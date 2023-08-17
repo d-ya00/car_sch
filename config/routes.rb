1eb39f62447d4fde53169185748c760ad088b38d
d-ya00 <daiya.koi@gmail.com>
2023-08-18 00:03:51 +0900

    ログアウト修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-   resources :users, only: :show
+   #resources :users, only: :show
   root to: 'blogs#index'
   resources :blogs, except: [:show] do
     get 'date/:date', to: 'blogs#show_by_date', on: :collection, as: 'show_by_date', constraints: { date: /\d{4}-\d{2}-\d{2}/ }
